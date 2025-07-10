b0f0e97c3b58c67b9c687011b7b4293a9344a71b
Yuriy-Yashkov <yuryman2019@mail.ru>
2025-07-11 00:23:49 +0300

    feat: Добавил имя для вывода

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,6 +5,6 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Hello from Jenkins! и Юрий!!!");
     }
 }
