b4ba56a8ed1b176feda42d75a3d77a435e229a77
Yuriy-Yashkov <yuryman2019@mail.ru>
2025-07-12 12:15:57 +0300

    feat: добавил паузу 20 сек на завершение.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import java.time.LocalTime;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start: " + LocalTime.now());
         System.out.println("Hello from Jenkins! и Юрий!!!");
+
+        Thread.sleep(20000);
+
+        System.out.println("The end !!!");
+        System.out.println("End: " + LocalTime.now());
     }
 }
